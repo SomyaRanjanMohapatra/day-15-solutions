13e21a00960cf0d5c2d1fa43d4b3e00dec8a87c9
SomyaRanjanMohapatra <62986681+SomyaRanjanMohapatra@users.noreply.github.com>
2020-07-01 22:10:28 +0530

    Add files via upload

---
+++ day 15 solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[50];
+    int ctr=1;
+    scanf("%[^\n]%*c",str);
+    for(int i=0;i<strlen(str);i++)
+    {
+        if(str[i]!=' ')
+        {
+            if (ctr%2==0)
+            {
+                --str[i];
+            }
+            else
+            {
+                ++str[i];
+            }
+        ctr++;
+        }
+    }
+    printf("%s",str);
+    return 0;
+}
